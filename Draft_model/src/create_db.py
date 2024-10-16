017d2b29e025db7270f8fae16ffdea96fbd60edc
EdoardoBucalo <edoardobucalo@gmail.com>
2024-10-16 18:40:51 +0200

    Aggiunte modifiche a create_db.py e rimosso create_db1.py, aggiunto create_db2.py

--- Draft_model/src/create_db.py
+++ Draft_model/src/create_db.py
@@ -1,18 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
 from scipy.io import readsav
 import numpy as np
 import os
+import sys
+sys.path.append('/Users/edoardo/Desktop/')
 import config
-from scipy.interpolate import BSpline, splrep 
 from tqdm import tqdm
+from scipy.interpolate import BSpline, splrep 
+
 
 # define a sample of an element in the database
 sample_dtype = np.dtype(
     [
-        ('label', np.str_, 10), # Label of the sample (ex: 30810_0140)
-        ('shot', np.int32), # Shot number (ex: 30810)
-        ('time', np.float32), # Time of the shot (ex: 0.0140)
-        ('data', np.float32, (92,)), # data in the bright struct the detectors (Line of sight)
-        ('data_err', np.float32, (92,)), # error on data in the bright struct (error on line of sight)
+        ('label', np.str_, 10), # Label of the sample
+        ('shot', np.int32), # Shot number
+        ('time', np.float32), # Time of the shot
+        ('data', np.float32, (92,)), # data in the bright struct the detectors
+        ('data_err', np.float32, (92,)), # error on data in the bright struct
         # Coordinates of the lines of sight, to be implemented
         # ('p', np.float32, (106,)), # impact parameters of the detectors l.o.s.
         # ('phi', np.float32, (106,)), # poloidal angles of the detectors l.o.s.
@@ -27,8 +36,8 @@ sample_dtype = np.dtype(
         ('emiss', np.float32, (110,110)), # Emissivity in the mesh
         ('x_emiss', np.float32, (110,)), # Vector of x coordinates for the mesh
         ('y_emiss', np.float32, (110,)), # Vector of y coordinates for the mesh
-        ('majr', np.float32), # Major radius of the machine (for rfx-mod is 2m)
-        ('minr', np.float32), # Minor radius of the plasma (for rfx-mod is 0.459m)
+        ('majr', np.float32), # Major radius of the plasma
+        ('minr', np.float32), # Minor radius of the plasma
         # Amplitudes and phases of the m = 1, n = -7 to -24 modes, b_tor are the
         # toroidal modes, b_rad are the radial modes, phi_tor are the phases of
         # the toroidal magnetic in the poloidal plane computed at 202.5 degrees,
@@ -45,7 +54,8 @@ def maximum_los():
     maximum number of lines of sight in the database
     '''
     # Maximum number of lines of sight
-    dir = "/home/orleans/projects/Tomography/Data/sav_files/"
+    dir = "/Users/edoardo/Desktop/Thesis/sav_files\ "
+
     n_max = 0
     for file in os.listdir(dir):
         if file.endswith('.sav'):
@@ -56,9 +66,33 @@ def maximum_los():
                 n_max = n_tot
     print(f'The maximum number of lines of sight in the db is {n_tot}')
 
+def discriminate_data(x, y, yerr):
+    '''
+    This function gets the x and y data (meaning the coordinates and the 
+    brilliances) from the .sav file and returns True if the data points in the y 
+    array (of the brilliance) are at distance less than a certain percentage of 
+    the reconstructed value via spline (tipically 10%) from the reconstructed 
+    value. At least 80% of the data points must be at a distance less than the
+    threshold in order for the profile to be kept. 
+    '''
+    # Compute the spline
+    tck = splrep(x, y, w=1/yerr, k=3, s=50)
+    # Compute the reconstructed value
+    y_reconstructed = BSpline(*tck)
+    # Compute the threshold
+    threshold = 0.1*np.max(y_reconstructed(x))
+    # Compute the distance between the reconstructed value and the data points
+    distance = np.abs(y - y_reconstructed(x))
+    # Compute the number of points that are at a distance less than the threshold
+    n_points = np.sum(distance < threshold)
+    # Check if the profile is to be kept
+    if n_points >= 0.8*len(y):
+        return True
+    else:
+        return False
 
 def read_tomography(file): 
-    dir = "../data/sav_files/"
+    dir = "/Users/edoardo/Desktop/Thesis/sav_files\  "
     # Load the data from the .sav file
     try:
         datum = readsav(dir + file)
@@ -132,6 +166,7 @@ def read_tomography(file):
         sample['minr'] = st_e['radius'][0]
     return sample[:i-k+1]
 
+
 def augment_data(logical_array, prel_array, data_array, error_array):
     # Create new arrays to store the modified data
     new_prel = []
@@ -165,37 +200,15 @@ def augment_data(logical_array, prel_array, data_array, error_array):
     # Return the modified coordinates
     return np.array(new_prel), np.array(new_data), np.array(new_error)
 
+
 def get_coefficients(file):
-    dir = "/home/orleans/projects/Tomography/Data/"
+    dir = "/Users/edoardo/Desktop/Thesis/sav_files\  "
     filename = os.join(dir, file)
     data = np.load(filename)
     # Now I have to compute the coefficients for each time instant in the .npy file
     pass
 
-def discriminate_data(x, y, yerr):
-    '''
-    This function gets the x and y data (meaning the coordinates and the 
-    brilliances) from the .sav file and returns True if the data points in the y 
-    array (of the brilliance) are at distance less than a certain percentage of 
-    the reconstructed value via spline (tipically 10%) from the reconstructed 
-    value. At least 80% of the data points must be at a distance less than the
-    threshold in order for the profile to be kept. 
-    '''
-    # Compute the spline
-    tck = splrep(x, y, w=1/yerr, k=3, s=25)
-    # Compute the reconstructed value
-    y_reconstructed = BSpline(*tck)
-    # Compute the threshold
-    threshold = 0.1*np.max(y_reconstructed(x))
-    # Compute the distance between the reconstructed value and the data points
-    distance = np.abs(y - y_reconstructed(x))
-    # Compute the number of points that are at a distance less than the threshold
-    n_points = np.sum(distance < threshold)
-    # Check if the profile is to be kept
-    if n_points >= 0.8*len(y):
-        return True
-    else:
-        return False
+
 
 def create_db():
     '''
@@ -204,7 +217,7 @@ def create_db():
     directory and saves them in a .npy file named data.npy
     '''
     data_dir = config.DATA_DIR
-    dir = '../data/sav_files/'
+    dir = "/Users/edoardo/Desktop/Thesis/sav_files\  "
     file = config.FILE_NAME
     if os.path.exists(data_dir + file):
         return 
@@ -218,12 +231,22 @@ def create_db():
         if len(data) != 0:
             data = np.concatenate(data)
         # Save data in npy format
+        os.makedirs(data_dir, exist_ok=True)
+
         np.save(os.path.join(data_dir, file), data)
         return
 
 if __name__ == "__main__":
     create_db()
     data = np.load(os.path.join(config.DATA_DIR, config.FILE_NAME))
-    print(data.shape)
-    print(len(data[0]))
+
+   
     
+    
+
+
+# In[ ]:
+
+
+
+
