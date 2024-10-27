e21301be4770a4f814e820505b8ccf536d4c8d0d
EdoardoBucalo <edoardobucalo@gmail.com>
2024-10-27 12:49:08 +0100

    cambio dataset

--- Draft_model/src/dataset.py
+++ Draft_model/src/dataset.py
@@ -6,6 +6,7 @@ import torch.utils
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 import create_db as c_db
+import utils
 
 # Togliere la dataset class e usare direttamente il datamodule?
 class TomographyDataset(torch.utils.data.Dataset):
@@ -27,19 +28,20 @@ class TomographyDataset(torch.utils.data.Dataset):
     self.time = bloated_dataset['time']
     self.dataerr = bloated_dataset['data_err']
     self.emiss = bloated_dataset['emiss']
-    self.x_emiss = bloated_dataset['x_emiss']
-    self.y_emiss = bloated_dataset['y_emiss']
-    self.majr = bloated_dataset['majr']
-    self.minr = bloated_dataset['minr']
+    self.x_emiss = bloated_dataset['x_emiss'][0]
+    self.y_emiss = bloated_dataset['y_emiss'][0]
+    self.majr = bloated_dataset['majr'][0]
+    self.minr = bloated_dataset['minr'][0]
     self.b_tor = bloated_dataset['b_tor']
     self.b_rad = bloated_dataset['b_rad']
     self.phi_tor = bloated_dataset['phi_tor']
+    self.j0, self.j1, self.em, self.em_hat, self.radii, self.angles = utils.compute_bessel_n_mesh(self.minr, self.majr, self.x_emiss, self.y_emiss)
 
   def __len__(self):
     return len(self.data)
 
   def __getitem__(self, idx):
-    return self.data[idx], self.target[idx]
+    return self.data[idx], self.target[idx], self.j0, self.j1, self.em, self.em_hat, self.radii, self.angles
 
 class TomographyDataModule(L.LightningDataModule):
   def __init__(self, data_dir, file_name, batch_size, num_workers=4):
@@ -58,42 +60,3 @@ class TomographyDataModule(L.LightningDataModule):
     '''
     # Generate the dataset if it does not exist yet
     c_db.create_db()
-
-  def setup(self, stage=None):
-    '''
-    The setup method is called before the dataloaders are created. Its main 
-    purpose is to:
-    - Load the dataset from the data directory. ()
-    - Specifically load only the brilliance and 
-    - split the dataset into training, validation, and test sets.
-    '''
-    # Load the dataset
-    entire_dataset = TomographyDataset(self.data_dir, self.file_name)
-    # Split the dataset into train, validation, and test sets
-    generator = torch.Generator().manual_seed(42) # Seed for reproducibility
-    # Here the actual split takes place, 80% for training, 10% for validation, and 10% for testing
-    self.train_ds, self.val_ds, self.test_ds = random_split(entire_dataset,
-                                              [0.8, 0.1, 0.1],
-                                              generator=generator)
-
-  def train_dataloader(self):
-    # Return the train dataloader
-    return DataLoader(self.train_ds,
-                      batch_size=self.batch_size,
-                      num_workers=self.num_workers,
-                      shuffle=True)
-
-  def val_dataloader(self):
-    # Return the validation dataloader
-    return DataLoader(self.val_ds,
-                      batch_size=self.batch_size,
-                      num_workers=self.num_workers,
-                      shuffle=False)
-
-  def test_dataloader(self):
-    # Return the test dataloader
-    return DataLoader(self.test_ds,
-                      batch_size=self.batch_size,
-                      num_workers=self.num_workers,
-                      shuffle=False)
-  
